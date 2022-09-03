eba6275ce633cc6d18985defa725fa9c7d5c2a6f
itsdaijoebu <joseph@josephchan.me>
2022-09-03 08:57:59 +0300

    spacing issues\n\nfixed some spacing issues

---
+++ models/TestModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const Hello = new mongoose.Schema({
+  todo: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Hello', Hello)
